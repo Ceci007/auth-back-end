ce396300983bca721c2e906a9f8705d3fd83af8e
Ceci007 <cecibenitezca@gmail.com>
2021-07-05 18:42:22 -0400

    Added first route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { readdirSync } from 'fs'
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -9,9 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('You hit server endpoint');
-});
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
 
