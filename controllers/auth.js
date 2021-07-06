49b2b100a949bff415f84d4b7081b6ae894d3ed8
Ceci007 <cecibenitezca@gmail.com>
2021-07-06 13:18:24 -0400

    Added first user with name, email and password fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,31 @@
-export const register = (req, res) => {
-  console.log(req.body);
-  res.json('register user endpoint, response from controller');
+import User from '../models/user'
+import { hashPassword, comparePassword } from '../utils/auth'
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if(!name) 
+      return res.status(400).send("Please enter your name");
+    if(!password || password.length < 6) 
+      return res.status(400).send("Please enter a valid password");
+
+    let userExist = await User.findOne({ email }).exec();
+    if(userExist)
+      return res.status(400).send("Email is taken");
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+      name, 
+      email, 
+      password: hashedPassword
+    })
+    await user.save();
+
+    return res.json({ ok: true });
+  } catch(err) {
+    console.log(err);
+    return res.status(400).send('Error, try again later');
+  }
 }
