9a50d68121969872a3f7f8e859e83e2af4848893
Ceci007 <cecibenitezca@gmail.com>
2021-07-07 15:02:49 -0400

    Added logout functionality to back-end

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,3 +57,13 @@ export const login = async (req, res) => {
     return res.status(400).send('Error, try again later');
   }
 }
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('token');
+    return res.json({ message: 'Logout successful'});
+  } catch(err) {
+    console.log(err);
+    return res.status(400).send('Error, try again later');
+  }
+}
