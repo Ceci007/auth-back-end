0a2ffe54ec3e96538c083b7165cf540fd072df78
Ceci007 <cecibenitezca@gmail.com>
2021-07-07 11:26:44 -0400

    Added login controller function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user'
 import { hashPassword, comparePassword } from '../utils/auth'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
   try {
@@ -24,6 +25,33 @@ export const register = async (req, res) => {
     await user.save();
 
     return res.json({ ok: true });
+  } catch(err) {
+    console.log(err);
+    return res.status(400).send('Error, try again later');
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).exec();
+
+    if(!user)
+      return res.status(400).send('No user found');
+    
+    const match = await comparePassword(password, user.password);
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    user.password = undefined;
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true
+    });
+
+    res.json(user);
   } catch(err) {
     console.log(err);
     return res.status(400).send('Error, try again later');
