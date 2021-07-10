d1b5ae854f3141f647803e5aa2f6bdd1bbd0523d
djimenez1388 <58003096+djimenez1388@users.noreply.github.com>
2021-07-10 13:43:14 -0400

    Configure csrf token

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import { readdirSync } from 'fs'
 import mongoose from 'mongoose'
+import csrf from 'csurf'
+import cookieParser from 'cookie-parser'
 const morgan = require('morgan');
 require('dotenv').config();
+
+const csrfProtection = csrf({ cookie: true });
 
 const app = express();
 
@@ -18,9 +22,16 @@ mongoose.connect(process.env.DATABASE, {
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+
+app.use(csrfProtection);
+
+app.get('/api/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 const port = process.env.PORT || 8000;
 
